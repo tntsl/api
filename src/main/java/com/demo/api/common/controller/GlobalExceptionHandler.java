4ea673232a5cac8ff512705215e2052dac78cc41
tntsl <tntsl1989com@sina.com>
2019-01-28 14:17:21 +0800

    修改全局错误处理信息返回

--- src/main/java/com/demo/api/common/controller/GlobalExceptionHandler.java
+++ src/main/java/com/demo/api/common/controller/GlobalExceptionHandler.java
@@ -28,12 +28,13 @@ public class GlobalExceptionHandler {
         if (e instanceof UnauthorizedException || e instanceof AuthorizationException) {
             return new Result().set403();
         } else if (e instanceof MethodArgumentNotValidException) {
-            MethodArgumentNotValidException exception = (MethodArgumentNotValidException) e;
-            BindingResult bindingResult = exception.getBindingResult();
+            MethodArgumentNotValidException methodArgumentNotValidException = (MethodArgumentNotValidException) e;
+            BindingResult bindingResult = methodArgumentNotValidException.getBindingResult();
             return new Result().set400().setMessage(ValidResultUtils.resultsToString(bindingResult));
         } else {
             LOGGER.error(e.getMessage(), e);
+            return new Result().set500().setMessage("请求发生错误，请稍后重试");
         }
-        return new Result().set500().setMessage("请求发生错误，请稍后重试");
     }
+
 }
